d28ef095f13156d5584eef779d455a82c97855fa
Douglas Cardoso <douglas.dcm@gmail.com>
2023-04-05 22:42:46 -0300

    testing workflow retry

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -4,7 +4,7 @@ from testtools import TestCase
 
 class TestWorkflows(TestCase):
 
-    @workflow_test(blueprint_path="test_blueprint.yaml")
+    @workflow_test(path_or_blueprint="test_blueprint.yaml")
     def test_workfow_demo(self, cfy_local):
         actual = cfy_local.execute('generic_workflow')
         self.assertIs(True, actual)
